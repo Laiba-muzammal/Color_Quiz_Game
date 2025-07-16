027e58e472d86657cf54ddf689a09b92a78a27c8
Laiba Muzammal <155936083+Laiba-muzammal@users.noreply.github.com>
2025-07-16 23:12:39 +0500

    Create questions.py

---
+++ questions.py
@@ -0,0 +1,9 @@
+questions = [
+    {"Q": "What is the color of the sky? \n1.blue\n2.green\n3.yellow\n4.black", "correct": "1"},
+    {"Q": "What is the color of the grass? \n1.blue\n2.green\n3.yellow\n4.black", "correct": "2"},
+    {"Q": "What is the color of the mango? \n1.blue\n2.green\n3.yellow\n4.black", "correct": "3"},
+    {"Q": "What is the color of the sun? \n1.blue\n2.green\n3.yellow\n4.black", "correct": "3"},
+    {"Q": "What is the color of the water? \n1.blue\n2.green\n3.yellow\n4.black", "correct": "1"},
+    {"Q": "What is the color of the leaves? \n1.blue\n2.green\n3.yellow\n4.black", "correct": "2"},
+    {"Q": "What is the color of the night? \n1.blue\n2.green\n3.yellow\n4.black", "correct": "4"}
+]
